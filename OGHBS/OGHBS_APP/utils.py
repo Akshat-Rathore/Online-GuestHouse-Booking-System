d10129b7955437e18566b39e97c23397f7f4df0a
Akshat-Rathore <akshatsinghrathore30@gmail.com>
2021-03-25 23:32:49 +0530

    Integrated User Dashboard

--- OGHBS/OGHBS_APP/utils.py
+++ OGHBS/OGHBS_APP/utils.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from six import text_type
-class AppToeknGenerator(PasswordResetTokenGenerator):
+
+class AppTokenGenerator(PasswordResetTokenGenerator):   #generates token based on parameters of user id and time of registration
     def _make_hash_value(self,user,timestamp):
         return (text_type(user.is_active) + text_type(user.pk)+text_type(timestamp))
 
-token_generator=AppToeknGenerator()
+token_generator=AppTokenGenerator()
